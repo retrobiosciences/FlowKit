18eee2084de1aaa610ab5fd778e175b6f2713fa4
Tobias Fehlmann <tobias@retro.bio>
2024-07-16 16:13:09 -0700

    Fix

--- setup.py
+++ setup.py
@@ -1,61 +1,54 @@
 """
 Setup script for the FlowKit package
 """
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 # read in version string
-VERSION_FILE = 'src/flowkit/_version.py'
+VERSION_FILE = "src/flowkit/_version.py"
 __version__ = None  # to avoid inspection warning and check if __version__ was loaded
 exec(open(VERSION_FILE).read())
 
 if __version__ is None:
     raise RuntimeError("__version__ string not found in file %s" % VERSION_FILE)
 
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 reqs = [
-    'anytree>=2.9',
-    'bokeh>=3.1',
-    'contourpy>=1.1.0',
-    'flowio>=1.3.0,<1.4',
-    'flowutils>=1.1.0,<1.2',
-    'lxml>=4.9',
-    'networkx>=3.1',
-    'numpy>=1.22,<2',
-    'pandas>=2.0',
-    'psutil>=5.8',
-<<<<<<< HEAD
-    'scipy>=1.8'
-||||||| parent of 75b9450 (Update setup.py)
-    'scipy>=1.6',
-    'seaborn>=0.11,<0.12'
-=======
-    'scipy>=1.6',
-    # 'seaborn>=0.11,<0.12' # REMOVING THIS AS WE'RE NOT USING IT
->>>>>>> 75b9450 (Update setup.py)
+    "anytree>=2.9",
+    "bokeh>=3.1",
+    "contourpy>=1.1.0",
+    "flowio>=1.3.0,<1.4",
+    "flowutils>=1.1.0,<1.2",
+    "lxml>=4.9",
+    "networkx>=3.1",
+    "numpy>=1.22,<2",
+    "pandas>=2.0",
+    "psutil>=5.8",
+    "scipy>=1.8",
 ]
 
 setup(
-    name='FlowKit',
+    name="FlowKit",
     version=__version__,  # noqa PyTypeChecker
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    package_data={'': ['_resources/*.xsd']},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={"": ["_resources/*.xsd"]},
     include_package_data=True,
-    description='Flow Cytometry Toolkit',
+    description="Flow Cytometry Toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Scott White",
-    license='BSD',
+    license="BSD",
     url="https://github.com/whitews/flowkit",
     ext_modules=[],
     install_requires=reqs,
     classifiers=[
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.8'
-    ]
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+    ],
 )
